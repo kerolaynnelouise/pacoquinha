5d772997b49911683b704a5a1c0da150c41adbb8
kerolaynne louise <karol.luzia0@gmail.com>
2024-10-02 20:39:28 -0300

    commit inicial

---
+++ src/main/java/com/senac/uc6/pacoquinha/PacoquinhaApplication.java
@@ -0,0 +1,13 @@
+package com.senac.uc6.pacoquinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacoquinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacoquinhaApplication.class, args);
+	}
+
+}
